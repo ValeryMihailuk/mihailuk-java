6ca185115fa82540e367886816d936051f2e891e
ValeryMihailuk <valery.mihkailuk@gmail.com>
2023-06-12 23:15:28 +0300

    refactor names class

--- src/main/java/Names.java
+++ src/main/java/Names.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class Names {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите имя: ");
+        String inputCheckName = input.nextLine();
+        if (inputCheckName.equals("Вячеслав")) {
+            System.out.println("Привет, Вячеслав");
+        } else {
+            System.out.println("Нет такого имени");
+        }
+    }
 }
